642df3bb8f9c19ba74bb0159f0ba5764c0979fe5
Gian Olivieri <gian.olivieri@gmail.com>
2015-08-29 01:03:03 -0430

    geo, views, jsons

--- bitely/www/js/app.js
+++ bitely/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('bitely', ['ionic','ionic.service.core','bitely.controllers','ngOpenFB'])
+angular.module('bitely', ['ionic','ionic.service.core','bitely.controllers','ngOpenFB', 'ngCordova'])
 
 .run(function($ionicPlatform, $rootScope, $location, $localstorage, ngFB) {
 
@@ -11,12 +11,12 @@ angular.module('bitely', ['ionic','ionic.service.core','bitely.controllers','ngO
 
 
   $rootScope.globals = $localstorage.getObject('globals') || {};
-  $rootScope.$on('$locationChangeStart', function (event, next, current) {
-    // SI NO ESTA EN EL SPLASH Y NO ESTA LOGGEADO
-    if ($location.path() !== '/splash' && !$rootScope.globals.currentUser) {
-        $location.path('/splash');
-    }
-  });
+  // $rootScope.$on('$locationChangeStart', function (event, next, current) {
+  //   // SI NO ESTA EN EL SPLASH Y NO ESTA LOGGEADO
+  //   if ($location.path() !== '/splash' && !$rootScope.globals.currentUser) {
+  //       $location.path('/splash');
+  //   }
+  // });
 
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -77,7 +77,78 @@ angular.module('bitely', ['ionic','ionic.service.core','bitely.controllers','ngO
         controller: 'PlateCtrl'
       }
     }
-  });
+  })
+
+  .state('app.editProfile', {
+    url:'/edit-profile',
+    views: {
+      'appContent': {
+        templateUrl : 'views/edit-profile.html',
+        controller: 'ProfileCtrl'
+      }
+    }
+  })
+
+  .state('app.addCard', {
+    url:'/add-card',
+    views: {
+      'appContent': {
+        templateUrl : 'views/add-card.html',
+        controller: 'CardCtrl'
+      }
+    }
+  })
+
+  .state('app.order', {
+    url:'/order',
+    abstract:true,
+    views: {
+      'appContent': {
+        templateUrl : 'views/order.html',
+        controller: 'OrderCtrl'
+      }
+    }
+  })
+
+  .state('app.order.personal', {
+    url:'/personal',
+    views: {
+      'orderContent': {
+        //controller: 'OrderCtrl',
+        templateUrl : 'views/order-personal.html'
+      }
+    }
+  })
+
+  .state('app.order.card', {
+    url:'/card',
+    views: {
+      'orderContent': {
+        //controller: 'OrderCtrl',
+        templateUrl : 'views/order-card.html'
+      }
+    }
+  })
+
+  .state('app.order.payment', {
+    url:'/payment',
+    views: {
+      'orderContent': {
+        //controller: 'OrderCtrl',
+        templateUrl : 'views/order-payment.html'
+      }
+    }
+  })   
+
+  .state('app.order.review', {
+    url:'/review',
+    views: {
+      'orderContent': {
+        //controller: 'OrderCtrl',
+        templateUrl : 'views/order-review.html'
+      }
+    }
+  })       
   
   $urlRouterProvider.otherwise('/app/home');
 
--- bitely/www/js/controllers.js
+++ bitely/www/js/controllers.js
@@ -1,5 +1,5 @@
 angular.module('bitely.controllers',['ngOpenFB'])
-.controller('AppCtrl', function($localstorage,$rootScope, $scope, $location, $ionicLoading, $timeout, $ionicPopup, $ionicSideMenuDelegate, ngFB) {
+.controller('AppCtrl', function($localstorage,$rootScope, $scope, $location, $ionicLoading, $timeout, $ionicPopup, ngFB) {
 
 	$rootScope.orderTotal = 0;
 
@@ -73,22 +73,41 @@ angular.module('bitely.controllers',['ngOpenFB'])
      		$localstorage.setObject('globals', {});
 			$rootScope.globals.currentUser = {};
 			$location.path('/splash');
-			$ionicSideMenuDelegate.toggleLeft();
      	}
    		});
  	};
 })
-.controller('HomeCtrl', function( $scope, $timeout) {
+.controller('HomeCtrl', function( $scope, $timeout, $http, $cordovaGeolocation) {
 
 	$scope.moreDataCanBeLoaded = true;
 
 	$scope.loaded = false;
 
-	$scope.location = {
+	$scope.user_loc = {};
+
+	var test_location = {
 		lat : "29.7377136",
         lon : "-95.58918089999997",
         radius : 200
     }
+	$scope.user_loc = test_location;
+
+
+	var posOptions = {timeout: 10000, enableHighAccuracy: false};
+
+
+  	
+  	$cordovaGeolocation.getCurrentPosition(posOptions)
+    .then(function (position) {
+		$scope.user_loc = {
+      		lat  : position.coords.latitude, long : position.coords.longitude, radius : 200
+  		}
+    }, function(err) {
+    	$scope.user_loc.error('no geoloc :(');
+    });
+
+
+
 
 	$scope.places = {};
 
@@ -96,94 +115,43 @@ angular.module('bitely.controllers',['ngOpenFB'])
 
 		$scope.loaded = true;
 
-		$scope.places = [
-			{
-				pid: 	1,
-				name: 	'Lorem ipsum dolor sit.',
-				image:  'img/restaurante.jpg',
-				street_address: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, sint.',
-				distance_to: '15'
-			},
-			{
-				pid: 	2,
-				name: 	'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit, fugiat, voluptatem impedit neque nam earum distinctio quidem tempore dicta praesentium.',
-				image:  'img/no_image.jpg',
-				street_address: '1346 West Liberty Ave.',
-				distance_to: '15'
-			},
-			{
-				pid: 	3,
-				name: 	'Lorem ipsum dolor sit.',
-				image:  'img/restaurante.jpg',
-				street_address: '1346 West Liberty Ave.',
-				distance_to: '15'
-			}
-		];
+		 $http.get('js/json/places.json').then(function(lugares){
+		 	$scope.places = lugares.data;
+		 });
 	}, 2000);
 
 	$scope.doRefresh = function() {
       $timeout(function() {
-		$scope.places = [
-			{
-				pid: 	1,
-				name: 	'NUEVO Lorem ipsum dolor sit.',
-				image:  'img/restaurante.jpg',
-				street_address: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, sint.',
-				distance_to: '15'
-			},
-			{
-				pid: 	2,
-				name: 	'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit, fugiat, voluptatem impedit neque nam earum distinctio quidem tempore dicta praesentium.',
-				image:  'img/restaurante.jpg',
-				street_address: '1346 West Liberty Ave.',
-				distance_to: '15'
-			},
-			{
-				pid: 	3,
-				name: 	'Lorem ipsum dolor sit.',
-				image:  'img/restaurante.jpg',
-				street_address: '1346 West Liberty Ave.',
-				distance_to: '15'
-			}
-		];
+
+      	$cordovaGeolocation.getCurrentPosition(posOptions)
+    	.then(function (position) {
+			$scope.user_loc = {
+	      		lat  : position.coords.latitude, long : position.coords.longitude, radius : 200
+	  		}
+    	}, function(err) {
+    		$scope.user_loc.error('no geoloc :(');
+    	});
+
+		 $http.get('js/json/places.json').then(function(lugares){
+		 	$scope.places = lugares.data;
+		 });
 
       }, 1000).finally(function() {
-       // Stop the ion-refresher from spinning
        $scope.$broadcast('scroll.refreshComplete');
      });
 	};
 	$scope.loadMore = function() {
   		$timeout(function() {
-	  		var morePlaces = [
-				{
-					pid: 	1,
-					name: 	'NUEVO Lorem ipsum dolor sit.',
-					image:  'img/restaurante.jpg',
-					street_address: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, sint.',
-					distance_to: '15'
-				},
-				{
-					pid: 	2,
-					name: 	'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit, fugiat, voluptatem impedit neque nam earum distinctio quidem tempore dicta praesentium.',
-					image:  'img/restaurante.jpg',
-					street_address: '1346 West Liberty Ave.',
-					distance_to: '15'
-				},
-				{
-					pid: 	3,
-					name: 	'Lorem ipsum dolor sit.',
-					image:  'img/restaurante.jpg',
-					street_address: '1346 West Liberty Ave.',
-					distance_to: '15'
-				}
-			];
+		 $http.get('js/json/places.json').then(function(lugares){
+		 	morePlaces = lugares.data;
   			$scope.places = $scope.places.concat(morePlaces);
 			$scope.$broadcast('scroll.infiniteScrollComplete');
+		 });
   		}, 1000);
   	};      
 })
 
-.controller('VenueCtrl', function($scope, $rootScope, $timeout){
+.controller('VenueCtrl', function($scope, $rootScope, $timeout, $ionicModal, $http){
 
 	$scope.venur = {};
 
@@ -192,206 +160,32 @@ angular.module('bitely.controllers',['ngOpenFB'])
 	$timeout(function(){
 		$scope.loaded = true;
 
-	$scope.venue = 
-		{
-			coordinates: {
-				lat:10.489279,
-				lon:-66.8720721
-			},
-			distance_to: 150,
-			name: "Lorem ipsum dolor sit amet",
-			num_tables:	3,
-			image:  'img/restaurante.jpg',
-			pid: 1,
-			street_address:	'1346 West Liberty Ave.',
-			description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, aliquam, accusamus consequuntur culpa ut sit modi veritatis eligendi nulla deleniti ipsam dolorem provident quasi porro sunt. Numquam quod magnam perspiciatis.',
-   			menus: [{
-   					'currency_symbol':'$',
-   					'sections': 
-   						[{ 
-   							'section_name': 'Section name',
-   							'subsections': [{ 
-   									'subsection_name': 'Subsection name',
-   									'contents' :[
-   									{
-   										'description': 'A rib-eye\u200f steak cooked with assorted vegetables in a special sauce and served on a hot sizzling platter.',
-   										'key': 'ahRzfnNvdW5kLWhlbHBlci04NzkyMXIRCxIESXRlbRiAgICA16X9Cgw',
-   										'name': 'ADD Oriental Sizzling Steak',
-   										'option_groups': [
-   										{
-   											'options': [
-   											{
-   												'name': 'Shrimp',
-   												'price': '+3.00'
-   											}],
-   											'text': 'Meat',
-   											'type': 'OPTION_ADD'
-   										}],
-   										'price': '16.95',
-                                        'type': 'ITEM'
-                                    },
-                                    {
-   											'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum nobis tempore omnis excepturi! Animi, a minima culpa suscipit rerum libero.',
-   											'key':	'burrito',
-   											'image': 'img/no_image.jpg',
-   											'name': 'CHOOSE Burrito',
-   											'price': 24,
-   											'type': 'ITEM',
-   											'option_groups': [
-   											{
-   												'options': [
-   												{
-   													'name': 'Hot and sour'
-   												},
-   												{
-   													'name': 'Wonton'
-   												},
-   												{
-   													'name': 'Egg drop soup'
-   												}],
-   												'text': 'Soup',
-   												'type': 'OPTION_CHOOSE'
-   											}]
-
-   									},{
-   											'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum nobis tempore omnis excepturi! Animi, a minima culpa suscipit rerum libero.',
-   											'key':	'burrito-otro',
-   											'image': 'img/plato.jpg',
-   											'name': 'Burrito Otro',
-   											'price': 24,
-   											'type': 'ITEM'
-   									}]
-   								},{ 
-   									'subsection_name': 'Subsection 2 name',
-   									'contents' :[{
-   											'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum nobis tempore omnis excepturi! Animi, a minima culpa suscipit rerum libero.',
-   											'key':	'tacos',
-   											'image': 'img/plato.jpg',
-   											'name': 'Tacos',
-   											'price': 20,
-   											'type': 'ITEM'
-   										},{
-   											'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum nobis tempore omnis excepturi! Animi, a minima culpa suscipit rerum libero.',
-   											'key':	'burrito',
-   											'image': 'img/plato.jpg',
-   											'name': 'Burrito',
-   											'price': 30,
-   											'type': 'ITEM'
-   									}]
-   								}]
-   							},{ 
-   							'section_name': 'Section two',
-   							'subsections': [{ 
-   									'subsection_name': 'Subsection two 1',
-   									'contents' :[{
-   											'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum nobis tempore omnis excepturi! Animi, a minima culpa suscipit rerum libero.',
-   											'key':	'tacos',
-   											'image': 'img/plato.jpg',
-   											'name': 'Tacos',
-   											'price': 20,
-   											'type': 'ITEM'
-   										},{
-   											'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum nobis tempore omnis excepturi! Animi, a minima culpa suscipit rerum libero.',
-   											'key':	'burrito',
-   											'image': 'img/plato.jpg',
-   											'name': 'Burrito',
-   											'price': 31,
-   											'type': 'ITEM'
-   									}]
-   								},{ 
-   									'subsection_name': 'Subsection Two 2',
-   									'contents' :[{
-   											'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum nobis tempore omnis excepturi! Animi, a minima culpa suscipit rerum libero.',
-   											'key':	'tacos',
-   											'image': 'img/plato.jpg',
-   											'name': 'Tacos',
-   											'price': 32,
-   											'type': 'ITEM'
-   										},{
-   											'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum nobis tempore omnis excepturi! Animi, a minima culpa suscipit rerum libero.',
-   											'key':	'burrito',
-   											'image': 'img/plato.jpg',
-   											'name': 'Burrito',
-   											'price': 20,
-   											'type': 'ITEM'
-   									},{
-   											'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum nobis tempore omnis excepturi! Animi, a minima culpa suscipit rerum libero.',
-   											'key':	'burrito',
-   											'image': 'img/plato.jpg',
-   											'name': 'Burrito',
-   											'price': 20,
-   											'type': 'ITEM'
-   									},{
-   											'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum nobis tempore omnis excepturi! Animi, a minima culpa suscipit rerum libero.',
-   											'key':	'burrito',
-   											'image': 'img/plato.jpg',
-   											'name': 'Burrito',
-   											'price': 20,
-   											'type': 'ITEM'
-   									}]
-   								}]
-   							}
-   						]
-   				},
-   				{ 
-   					'sections': 
-   						[{ 
-   							'section_name': 'Section name 2',
-   							'subsections': [{ 
-   									'subsection_name': '2 Another one',
-   									'contents' :[{
-   											'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum nobis tempore omnis excepturi! Animi, a minima culpa suscipit rerum libero.',
-   											'key':	'tacos',
-   											'image': 'img/plato.jpg',
-   											'name': 'Tacos',
-   											'price': 20,
-   											'type': 'ITEM'
-   										}]
-   								},{ 
-   									'subsection_name': '2 Another one 2',
-   									'contents' :[{
-   											'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum nobis tempore omnis excepturi! Animi, a minima culpa suscipit rerum libero.',
-   											'key':	'tacos',
-   											'image': 'img/plato.jpg',
-   											'name': 'Tacos',
-   											'price': 20,
-   											'type': 'ITEM'
-   										},{
-   											'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum nobis tempore omnis excepturi! Animi, a minima culpa suscipit rerum libero.',
-   											'key':	'burrito',
-   											'image': 'img/plato.jpg',
-   											'name': 'Burrito',
-   											'price': 30,
-   											'type': 'ITEM'
-   									}]
-   								}]
-   							},{ 
-   							'section_name': '22 Some other name 2',
-   							'subsections': [{ 
-   									'subsection_name': '22 Another 2 one',
-   									'contents' :[ {
-   											'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum nobis tempore omnis excepturi! Animi, a minima culpa suscipit rerum libero.',
-   											'key':	'tacos',
-   											'image': 'img/plato.jpg',
-   											'name': 'Tacos',
-   											'price': 20,
-   											'type': 'ITEM'
-   										},{
-   											'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum nobis tempore omnis excepturi! Animi, a minima culpa suscipit rerum libero.',
-   											'key':	'burrito',
-   											'image': 'img/plato.jpg',
-   											'name': 'Burrito',
-   											'price': 31,
-   											'type': 'ITEM'
-   									}]
-   								}]
-   							}   							
-   						]
-   				}]
-		};
+		 $http.get('js/json/venue.json').then(function(lugares){
+		 	$scope.venue = lugares.data;
+		 });
+		
 	}, 2000);
+
+	$ionicModal.fromTemplateUrl('views/addtoorder.html', {
+		scope: $scope
+	}).then(function(modal) {
+		$scope.addModal = modal;
+	});
+	$scope.closeAddModal = function() {
+		$scope.addModal.hide();
+	};
+	$scope.sendOrder = function(key, option_groups){
+		if (option_groups) {
+		 	$scope.addModal.options = option_groups;
+			$scope.addModal.show();
+		} else {
+
+		}
+		
+	}
+
 })
-.controller('PlateCtrl', function( $scope, $timeout) {
+.controller('PlateCtrl', function( $scope, $timeout, $http) {
 
 	$scope.plate = {};
 
@@ -399,38 +193,73 @@ angular.module('bitely.controllers',['ngOpenFB'])
 
 	$timeout(function(){
 		$scope.loaded = true;
-
-
-	$scope.plate = {
-		description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis, et temporibus explicabo velit alias rerum impedit at totam. Ratione, non!",
-		distance_to:15,	
-		image: 'img/plato.jpg',
-		key: "uihkjggjk",
-		location:	{
-			lat:10.489279,
-			lon:-66.8720721
-		},
-		name: "Plate Lorem ipsum.",
-		pid: 123123,
-		price: 200,
-		rating_average:	3.5,
-		restaurant:	{
-			coordinates: {
-				lat:10.489279,
-				lon:-66.8720721
-			},
-			distance_to: 15,
-			name: "Rest Lorem ipsum dolor ",
-			num_tables:	30,
-			pid: 23123,
-			street_address:	"Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-		},
-		score: 8
-	}		
+		 $http.get('js/json/plate.json').then(function(lugares){
+		 	$scope.plate = lugares.data;
+		 });
 
 	}, 2000);
 
 
 
+})
+
+.controller('ProfileCtrl', function($scope, $timeout){
+	$scope.profile = {};
+	$scope.profile.data = {};
+	$scope.profile.data.state ='';
+
+	$scope.$watch("profile.data.state", function(newValue, oldValue){
+        if (newValue.length > 2){
+            $scope.profile.data.state = oldValue;
+        }
+    });
+
+	$scope.saveProfile = function() {
+		alert('saved!');
+	};
+
+})
+
+.controller('CardCtrl', function($scope, $timeout){
+	$scope.card = {};
+	$scope.card.data = {};
+
+
+	$scope.card.data.number ='';
+
+	$scope.$watch("card.data.number", function(newValue, oldValue){
+        if (newValue.length > 19){
+            $scope.card.data.number = oldValue;
+        }
+    });
+
+	$scope.card.data.cvc ='';
+
+	$scope.$watch("card.data.cvc", function(newValue, oldValue){
+        if (newValue.toString().length > 4){
+            $scope.card.data.cvc = oldValue;
+        }
+    });    
+
+	$scope.saveCard = function() {
+		alert('saved!');
+	};
+
+})
+
+.controller('OrderCtrl', function($scope, $timeout, $location){
+
+	$scope.goCard = function(){
+		$location.path('/app/order/card');
+	}
+	$scope.goPayment = function(){
+		$location.path('/app/order/payment');
+	}
+	$scope.goReview = function(){
+		$location.path('/app/order/review');
+	}
+	$scope.goHome = function(){
+		$location.path('/app');
+	}
 })
 ;
