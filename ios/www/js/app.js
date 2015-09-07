e9f1fd987c99946f7cebd1c448375793ba6c1e63
gianko <gian.olivieri@gmail.com>
2015-09-07 18:16:42 -0430

    iOs Xproject

---
+++ ios/www/js/app.js
@@ -0,0 +1,181 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('bitely', ['ionic','ionic.service.core','ionic.rating','bitely.controllers', 'ngCordova', 'ngResource', 'ngCookies', 'angularPayments'])
+
+.run(function($cordovaStatusbar, $cookies, $ionicPlatform, $rootScope, $location, $localstorage, $window) {
+  // 1646690858946373
+  // cookie: session
+  
+  //ngFB.init({appId: '1397986197168446'});
+
+
+  $window.Stripe.setPublishableKey('pk_test_knVqvEMFxZsgserDdUhovk24');
+
+  $rootScope.globals = $localstorage.getObject('globals') || {};
+  $rootScope.order   = $localstorage.getObject('order') || {};
+  $rootScope.creditcard   = $localstorage.getObject('creditcard') || {};
+  $rootScope.$on('$locationChangeStart', function (event, next, current) {
+    // SI NO ESTA EN EL SPLASH Y NO ESTA LOGGEADO
+    if ($location.path() !== '/splash' && !$rootScope.globals.currentUser) {
+        $location.path('/splash');
+    }
+  });
+
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    // if(window.StatusBar) {
+      //StatusBar.styleDefault();
+      // $cordovaStatusbar.styleHex('#fb7d00');
+    // }
+  if (window.StatusBar) {
+    if (ionic.Platform.isAndroid()) {
+      StatusBar.backgroundColorByHexString('#c96401');
+    } else {
+      // StatusBar.styleLightContent();
+    }
+  }    
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+ 
+
+ $httpProvider.defaults.withCredentials = true;
+
+  //ROUTES
+  $stateProvider
+
+  .state('splash', {
+    url: '/splash',
+    templateUrl: 'views/splash.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app',{
+    url: '/app',
+    abstract: true,
+    templateUrl: 'views/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.home', {
+    url: '/home',
+    views: {
+      'appContent':{
+        templateUrl: "views/home.html",
+        controller: 'HomeCtrl'
+      }
+    }
+  })
+
+  .state('app.venue', {
+    url: '/menu/:rest_id/:name_id',
+    views: {
+      'appContent': {
+        templateUrl: 'views/venue.html',
+        controller: 'VenueCtrl'
+      }
+    }
+  })
+
+  .state('app.plate', {
+    url: '/plate/:id',
+    views: {
+      'appContent': {
+        templateUrl: 'views/plate.html',
+        controller: 'PlateCtrl'
+      }
+    }
+  })
+
+  .state('app.editProfile', {
+    url:'/edit-profile',
+    views: {
+      'appContent': {
+        templateUrl : 'views/edit-profile.html',
+        controller: 'ProfileCtrl'
+      }
+    }
+  })
+
+  .state('app.addCard', {
+    url:'/add-card',
+    views: {
+      'appContent': {
+        templateUrl : 'views/add-card.html',
+        controller: 'CardCtrl'
+      }
+    }
+  })
+
+  .state('app.order', {
+    url:'/order',
+    abstract:true,
+    views: {
+      'appContent': {
+        templateUrl : 'views/order.html',
+        controller: 'OrderCtrl'
+      }
+    }
+  })
+
+  .state('app.order.personal', {
+    url:'/personal',
+    views: {
+      'orderContent': {
+        //controller: 'OrderCtrl',
+        templateUrl : 'views/order-personal.html'
+      }
+    }
+  })
+
+  .state('app.order.card', {
+    url:'/card',
+    views: {
+      'orderContent': {
+        //controller: 'OrderCtrl',
+        templateUrl : 'views/order-card.html'
+      }
+    }
+  })
+
+  .state('app.order.confirm', {
+    url:'/confirm',
+    views: {
+      'orderContent': {
+        //controller: 'OrderCtrl',
+        templateUrl : 'views/order-confirm.html'
+      }
+    }
+  })   
+
+  .state('app.order.payment', {
+    url:'/payment',
+    views: {
+      'orderContent': {
+        //controller: 'OrderCtrl',
+        templateUrl : 'views/order-payment.html'
+      }
+    }
+  })  
+  .state('app.order.success', {
+    url:'/success',
+    views: {
+      'orderContent': {
+        //controller: 'OrderCtrl',
+        templateUrl : 'views/order-success.html'
+      }
+    }
+  })  
+  
+  $urlRouterProvider.otherwise('/app/home');
+
+
+});
---
+++ ios/www/js/services.js
@@ -0,0 +1,134 @@
+var urlBase = "https://www.bitely.io/bitely/api/v3.0/";
+
+angular.module('bitely.controllers')
+.factory('$localstorage', ['$window', function($window) {
+  return {
+    set: function(key, value) {
+      $window.localStorage[key] = value;
+    },
+    get: function(key, defaultValue) {
+      return $window.localStorage[key] || defaultValue;
+    },
+    setObject: function(key, value) {
+      $window.localStorage[key] = JSON.stringify(value);
+    },
+    getObject: function(key) {
+      return JSON.parse($window.localStorage[key] || '{}');
+    },
+    remove: function(key) {
+      return $window.localStorage.removeItem(key);
+    }
+  }
+}])
+
+.factory('Auth', function($localstorage, $rootScope, $cookies){
+  var service = {};
+
+  service.setCredentials = function(todalainfo){
+      $rootScope.globals = {
+          currentUser: todalainfo
+      };
+      $localstorage.setObject('globals',{currentUser:todalainfo});
+  }
+
+  service.clearCredentials = function(){
+      $rootScope.globals = {};
+      $localstorage.setObject('globals',{});
+      $cookies.remove("session");
+      $rootScope.creditcard = {};
+      $localstorage.setObject('creditcard',{});
+      $rootScope.order = {};
+      $localstorage.setObject('order',{});
+  }
+
+  return service;
+})
+
+
+.factory('Venues', function($resource){
+  return $resource(urlBase+'venue_list');
+})
+
+.factory('Venue', function($resource){
+  return $resource(urlBase+'venue');
+})
+
+.factory('Menu', function($resource){
+  return $resource(urlBase+'menu');
+})
+.factory('User', function($resource){
+  return $resource(urlBase+'user', null, {
+    'save': {
+        method:'POST',
+        headers:{'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'}
+    }
+  });
+})
+
+.factory('Pay', function($resource){
+  return $resource(urlBase+'pay', null, {
+    'save': {
+        method:'POST',
+        headers:{'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'}
+    }
+  });
+})
+
+.factory('gLocation', function($resource){
+  return $resource(urlBase+'location', null, {
+        'update': { 
+          method:'PUT',
+         }
+  });
+})
+
+.factory('Rating', function($resource){
+  return $resource(urlBase+'rating', null, {
+        'update': { 
+          method:'PUT',
+         }
+  });
+})
+.factory('Order', function($resource, $localstorage, $rootScope){
+  return $resource(urlBase+'order', null, {
+        'update': { 
+          method:'PUT',
+          interceptor: {
+            response: function(order){
+              console.log('order update:', order);
+              $rootScope.order = order.data;
+              $localstorage.setObject('order',order.data);
+            },
+            responseError: function (data) {
+              console.log('error in interceptor', data);
+            }
+          }
+         },
+        'query': {
+          method:'GET',
+          interceptor: {
+            response: function(order){
+              console.log('order query:', order);
+              $rootScope.order = order.data;
+              $localstorage.setObject('order',order.data);
+            },
+            responseError: function (data) {
+              console.log('error in interceptor', order.data);
+            }
+          }
+        },
+        'save': {
+          method:'POST',
+          interceptor: {
+            response: function(order){
+              console.log('order save:', order);
+              $rootScope.order = order.data;
+              $localstorage.setObject('order',order.data);
+            },
+            responseError: function (data) {
+              console.log('error in interceptor', order.data);
+            }
+          }
+        }        
+  });
+})
